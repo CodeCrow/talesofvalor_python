e532280b96d30587366bdd69697220d30f87ed14
Rob Archer <robert_archer@wgbh.org>
2019-06-02 14:28:21 -0400

    fix passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -12,6 +12,6 @@ sys.path.insert(0,'$HOME/.virtualenvs/talesofvalor/bin')
 sys.path.insert(0,'$HOME/.virtualenvs/talesofvalor/lib/python2.7/site-packages/django')
 sys.path.insert(0,'$HOME/.virtualenvs/talesofvalor/lib/python2.7/site-packages')
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'talesofvalor.settings.production'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'talesofvalor.settings.stage'
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
