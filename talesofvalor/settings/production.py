0a1a773d471ca37c06da055339549542478bcd37
Rob Archer <robert_archer@wgbh.org>
2020-01-12 22:04:37 -0500

    initalize strict mode

--- talesofvalor/settings/production.py
+++ talesofvalor/settings/production.py
@@ -16,7 +16,7 @@ DATABASES = {
         'PASSWORD': 'wereunicorn',
         'HOST': 'mysql.talesofvalor.com',
         'OPTIONS': {
-            'init_command': 'SET innodb_strict_mode=1',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES',innodb_strict_mode=1",
         },
     }
 }
