9bbf0298b2d8167a68d33c8f962582eb50ad9ab6
Rob Archer <robert_archer@wgbh.org>
2021-07-27 19:31:43 -0400

    adding pel displays

--- talesofvalor/betweengameskills/views.py
+++ talesofvalor/betweengameskills/views.py
@@ -3,6 +3,7 @@ These are views that are used for viewing and editing Between game skills.
 
 BGS are written by players and .
 """
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin,\
     PermissionRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
@@ -13,7 +14,6 @@ from django.views.generic import DetailView, ListView
 from talesofvalor.events.models import Event
 
 from .models import BetweenGameSkill
-
 
 
 class BetweenGameSkillCreateView(LoginRequiredMixin, CreateView):
@@ -38,7 +38,6 @@ class BetweenGameSkillCharacterEventView(
     template_name = "betweengameskill/characterevent_detail.html"
 
 
-
 class BetweenGameSkillUpdateView(
         LoginRequiredMixin,
         PermissionRequiredMixin,
@@ -59,7 +58,7 @@ class BetweenGameSkillUpdateView(
         try:
             bgs = BetweenGameSkill.objects.get(self.kwargs['pid'])
             return (bgs.character.player == self.request.user)
-        except DoesNotExist:
+        except BetweenGameSkill.DoesNotExist:
             return False
         return False
 
@@ -83,9 +82,10 @@ class BetweenGameSkillDeleteView(
         try:
             bgs = BetweenGameSkill.objects.get(self.kwargs['pid'])
             return (bgs.character.player == self.request.user)
-        except DoesNotExist:
+        except BetweenGameSkill.DoesNotExist:
             return False
         return False
+
 
 class BetweenGameSkillDetailView(LoginRequiredMixin, DetailView):
     """
@@ -100,10 +100,34 @@ class BetweenGameSkillDetailView(LoginRequiredMixin, DetailView):
 
 class BetweenGameSkillListView(LoginRequiredMixin, ListView):
     """
-    Show the details for a character.
+    Show a list of BetweenGameSkills.
 
-    From here you can edit the details of a character or choose skills.
+    Limit by event if the event id is sent.
+    Limit by player if the player username is sent.
+    If the player username is not sent, limit the list to the current user
+    if they don't have the permission to "view_any_player"
+
+    There should be a modal to add comments to each BGS
     """
 
     model = BetweenGameSkill
     paginate_by = 25
+    ordering = ['-event__event_date', 'character']
+
+    def get_queryset(self):
+        user = None
+        queryset = super().get_queryset()
+        # filter by event
+        if self.request.GET.get('event', None):
+            queryset = queryset.filter(event=self.request.GET['event'])
+        # now filter based on what they are allowed to see
+        if self.request.user.has_perm('player.view_any_player'):
+            # if the username was sent, filter by that.
+            if self.request.GET.get('username', None):
+                user = User.objects.get(username=self.request.GET['username'])
+        else:
+            user = self.request.user
+        if user:
+            characters = user.player.character_set()
+            queryset = queryset.filter(character__in=characters)
+        return queryset
