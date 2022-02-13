e329e9f43143f542c86e8ea3458543ddeba63b71
Rob Archer <robert_archer@wgbh.org>
2022-02-12 20:04:30 -0500

    does that column exist?

--- talesofvalor/players/migrations/0021_auto_20220124_1753.py
+++ talesofvalor/players/migrations/0021_auto_20220124_1753.py
@@ -11,10 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='pel',
-            name='data',
-        ),
         migrations.AddField(
             model_name='pel',
             name='devout',
