6cf68df40e521723475cc6470c680ae4710ed73a
Rob Archer <robert_archer@wgbh.org>
2024-01-11 14:54:04 -0500

    add the Javascript for making comments to the BGA

--- talesofvalor/comments/templatetags/comment_tags.py
+++ talesofvalor/comments/templatetags/comment_tags.py
@@ -5,6 +5,7 @@ easily.
 from django import template
 from django.contrib.contenttypes.models import ContentType
 from django.template.loader import get_template
+from django.utils.safestring import mark_safe
 
 from ..models import Comment
 
@@ -16,8 +17,41 @@ t = get_template("comments/includes/comment_list.html")
 def comments_display(obj):
     obj_type = ContentType.objects.get_for_model(obj)
     comments = Comment.objects.filter(content_type=obj_type, object_id=obj.id)
-    print(f"COMMENTS:{comments}")
     return {
         'object': obj,
         'comments': comments
     }
+
+
+@register.simple_tag
+def comment_add():
+    """
+    Write out the javascript that creates a for for a new Comment
+    and then sends out the form submission.
+    """
+    add_script = """
+    $('.add-comment').click((e)=>{
+        let objectId = $(e.currentTarget).data('objectid');
+        console.log($(e.currentTarget).data('objectid'));
+        $comment_form = $('<form>');
+        $comment_form.append($('<h5>Add a Comment:</h5>'));
+        $comment_form.append($('<textarea>'));
+        $comment_form.append($('<br>'));
+        $comment_form.append($('<input type="button" value="save" class="submit-comment btn btn-primary">'));
+        console.log($comment_form);
+        console.log($('#controls_comment_' + objectId));
+        $('#controls_comment_' + objectId).append($comment_form);
+        $(e.currentTarget).remove();
+    });
+    """
+    return mark_safe(add_script)
+
+
+@register.simple_tag
+def comment_delete():
+    """
+    Write out the javascript that deletes a comment..
+    """
+    delete_script = """
+    """
+    return mark_safe(delete_script)
